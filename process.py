19b6a1a7ae24bd3f961dd38e3ad2697b14ce460b
Law Po Ying <30721578+yingying0906@users.noreply.github.com>
2023-12-03 11:54:39 +0800

    post processing code update
    - filter invalid prediction
    - time normalization debug

--- process.py
+++ process.py
@@ -3,18 +3,32 @@ from datetime import datetime
 # type list
 typeList = ["PATIENT", "DOCTOR", "USERNAME", "PROFESSION", "ROOM", "DEPARTMENT", "HOSPITAL", "ORGANIZATION", "STREET", "CITY", "STATE", "COUNTRY", "ZIP", "LOCATION-OTHER", "AGE", "DATE", "TIME", "DURATION", "SET","PHONE", "FAX", "EMAIL", "URL", "IPADDR", "SSN", "MEDICALRECORD", "HEALTHPLAN", "ACCOUNT", "LICENSE", "VECHICLE", "DEVICE", "BIOID", "IDNUM" ]
 
-input_file_path = "answer.txt"
-write_file_path = "answer_out.txt"
+input_file_path = "mj2-neglo-500-ep10_test.out"
+write_file_path = "answer.txt"
     
 def convert_date(date_str):
     try:
         # year only
-        if len(date_str) == 4:
-            return date_str
-        else:
-            # all date
-            date_obj = parser.parse(date_str, dayfirst=True, yearfirst=False)
-            return date_obj.strftime("%Y-%m-%d")
+        # not completed year
+        if "/" in date_str:
+            date_parts = date_str.split("/")
+            if len(date_parts) < 3:
+                return "FAIL"
+            for element in date_parts:
+                if element == '':
+                    return "FAIL"
+        elif "." in date_str:
+            date_parts = date_str.split(".")
+            if len(date_parts) < 3:
+                return "FAIL"
+            for element in date_parts:
+                if element == '':
+                    return "FAIL"
+        if date_str.count("/") < 2 and date_str.count("-") < 2 and date_str.count(".") < 2:
+            return "FAIL"
+        # all date
+        date_obj = parser.parse(date_str, dayfirst=True, yearfirst=False)
+        return date_obj.strftime("%Y-%m-%d")
     except ValueError:
         try:
             # ignore useless symbol
@@ -61,7 +75,7 @@ def convert_datetime(datetime_str):
         except ValueError:
             pass
 
-    return datetime_str
+    return "FAIL"
 
 def convert_duration(duration_str):
     duration_mapping = {
@@ -88,7 +102,7 @@ def convert_duration(duration_str):
         if unit in duration_mapping:
             return f"P{num}{duration_mapping[unit]}"
     
-    return duration_str
+    return "FAIL"
 
 # read write file
 with open(input_file_path, 'r') as input_file, open(write_file_path, 'w') as output_file:
@@ -96,24 +110,64 @@ with open(input_file_path, 'r') as input_file, open(write_file_path, 'w') as out
     while line:
         # filter useless type
         lineSplit = line.strip().split("\t")
-        if lineSplit[1] not in typeList:
+
+        # filter wrong time related format
+        if (lineSplit[1] == "DATE" or lineSplit[1] == "TIME" or lineSplit[1] == "DURATION" or lineSplit[1] == "SET") and (len(lineSplit) < 6): # if time info < 6
             print(line)
-        else:
-            #  datetime normalization
-            if lineSplit[1] == "DATE":
-                converted = convert_date(lineSplit[4])
-                lineSplit[5] = converted
-                output_file.write('\t'.join(lineSplit) + '\n')
-            elif lineSplit[1] == "TIME":
-                converted = convert_datetime(lineSplit[4])
-                lineSplit[5] = converted
-                output_file.write('\t'.join(lineSplit) + '\n')
-            elif lineSplit[1] == "DURATION":
-                converted = convert_duration(lineSplit[4])
-                lineSplit[5] = converted
-                output_file.write('\t'.join(lineSplit) + '\n')
+        elif lineSplit[1] not in typeList: # if type not exist
+            print(line)
+        elif lineSplit [1] == "DOCTOR" or lineSplit [1] == "PATIENT" or lineSplit [1] == "STREET" or lineSplit [1] == "STATE":
+            if len(lineSplit[4]) < 2:
+                print(line)
             else:
                 output_file.write(line)
+        elif lineSplit [1] == "IDNUM" or lineSplit [1] == "DEPARTMENT":
+            if len(lineSplit[4]) < 3:
+                print(line)
+            else:
+                output_file.write(line)
+        elif lineSplit [1] == "CITY" or lineSplit [1] == "ZIP":
+            if len(lineSplit[4]) < 4:
+                print(line)
+            else:
+                output_file.write(line)
+        elif lineSplit [1] == "COUNTRY" or lineSplit [1] == "ORGANIZATION":
+            if len(lineSplit[4]) < 5:
+                print(line)
+            else:
+                output_file.write(line)
+        elif lineSplit [1] == "MEDICALRECORD":
+            if len(lineSplit[4]) < 6:
+                print(line)
+            else:
+                output_file.write(line)
+        elif lineSplit [1] == "HOSPITAL":
+            if len(lineSplit[4]) < 7:
+                print(line)
+            else:
+                output_file.write(line)   
+        else:
+            #  datetime normalization
+            converted = ""
+            if lineSplit[1] == "DATE":
+                converted = convert_date(lineSplit[4])
+            elif lineSplit[1] == "TIME":
+                converted = convert_datetime(lineSplit[4])
+                if "T" in lineSplit[5]:
+                    converted = "FAIL"
+            elif lineSplit[1] == "DURATION":
+                converted = convert_duration(lineSplit[4])
+            elif lineSplit[1] == "SET":
+                converted = "FAIL"
+
+            # if cannot convert
+            if converted != "FAIL":
+                # change/append the answer
+                if len(lineSplit) == 6:
+                    lineSplit[5] = converted
+            # write
+            output_file.write(('\t'.join(lineSplit)).strip() + '\n')
+
         # next line
         line = input_file.readline()
         
